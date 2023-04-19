3ebe8d3d22801dc16a682653f6430beb66022095
Francisco  Gerardi <franciscogerardi@gmail.com>
2023-04-19 16:07:45 +0300

    added map function and onchage to SuperSimple

--- vercel_app/components/SuperSimple.tsx
+++ vercel_app/components/SuperSimple.tsx
@@ -1,8 +1,6 @@
 import * as React from "react";
 import { Range } from "react-range";
-import { FC } from "react";
-
-
+import { FC, useEffect, useState } from "react";
 
 interface SliderProps {
     midi: number[];
@@ -11,43 +9,60 @@ interface SliderProps {
 const SuperSimple: FC<SliderProps> = ({ midi }) => {
     //state = { values: [50] };
 
-    const handleSlider =()=>{
+    const [values, setValues] = useState(50);
+    const [command, setCommand] = useState(0);
+    const [note, setNote] = useState(0);
+    const [velocity, setVelocity] = useState();
 
-    }
+    useEffect(() => {
+        if (midi[1] == note) {
+            setValues(midi[2]);
+        }
 
+        return () => {};
+    }, [midi]);
+
+    const handleClick = () => {
+        setNote(midi[1]);
+    };
 
     return (
-        <Range
-            step={0.1}
-            min={0}
-            max={100}
-            values={[midi[2]]}
-            //onChange={handleSlider()}
-            renderTrack={({ props, children }) => (
-                <div
-                    {...props}
-                    style={{
-                        ...props.style,
-                        height: "6px",
-                        width: "100%",
-                        backgroundColor: "#ccc",
-                    }}
-                >
-                    {children}
-                </div>
-            )}
-            renderThumb={({ props }) => (
-                <div
-                    {...props}
-                    style={{
-                        ...props.style,
-                        height: "42px",
-                        width: "42px",
-                        backgroundColor: "#999",
-                    }} />
-            )} onChange={function (values: number[]): void {
-                throw new Error("Function not implemented.");
-            } }        />
+        <>
+            <Range
+                step={0.1}
+                min={0}
+                max={100}
+                values={[values]}
+                onChange={(values) => setValues(values)}
+                renderTrack={({ props, children }) => (
+                    <div
+                        {...props}
+                        style={{
+                            ...props.style,
+                            height: "6px",
+                            width: "100%",
+                            backgroundColor: "#ccc",
+                        }}
+                    >
+                        {children}
+                    </div>
+                )}
+                renderThumb={({ props }) => (
+                    <div
+                        {...props}
+                        style={{
+                            ...props.style,
+                            height: "42px",
+                            width: "42px",
+                            backgroundColor: "#999",
+                        }}
+                    />
+                )}
+            />
+            <button type="button" onClick={handleClick}>
+                {note ? note : "MAP"}
+            </button>
+        </>
     );
 };
 
